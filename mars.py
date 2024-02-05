43ba027cc3bec08995d4ad7ca1e94bca1f987dae
Ian McKinnon <ian.s.mckinnon-1@ou.edu>
2024-02-05 17:13:29 -0600

    Created data plot

--- mars.py
+++ mars.py
@@ -1,6 +1,9 @@
 # NAME description of the code I'm writing
 
 from pandas import read_csv
+import matplotlib.pyplot as plt
 
 #Read in data
-read_csv('mars_elements.csv')
+data = read_csv('mars_elements.csv')
+
+plt.show(data)
